313efc4b4bf5b6fcb37cba1389c19b90a1ac81ac
Liam Flanagan <liam.flanagan@capitalontap.com>
2018-11-02 18:24:59 +0000

    Added appveyor yaml file for testing Added parallel build script

---
+++ msbuild/build-parallel.ps1
@@ -0,0 +1,37 @@
+function buildContainer {
+    param (
+        [string]$ScriptPath
+        ,[string]$Variant
+        ,[bool]$PushToRepository = $false
+    )
+    Write-Information "Building $Variant"
+    $variantPath = "$ScriptPath/$Variant"
+
+    # Build docker image
+    docker build -t "lflanagan/msbuild:$Variant" -f "$variantPath/Dockerfile" "$variantPath"
+
+    # Push image(s) to docker hub
+    if ($true -eq $PushToRepository){
+        docker push "lflanagan/msbuild:$Variant"
+    }
+}
+
+workflow buildContainers {
+    param (
+        [string]$ScriptPath
+        ,[string[]]$Variants
+    )
+
+    foreach -parallel ($variant in $Variants) {
+        buildContainer -ScriptPath $ScriptPath -Variant $variant
+    }
+}
+
+# Get list of variants
+$variants = Get-ChildItem | Where-Object {$_.PSIsContainer} | Foreach-Object {$_.Name}
+
+# Get script path
+$scriptPath = $PSScriptRoot
+
+# Run!
+buildContainers -ScriptPath $scriptPath -Variants $variants
