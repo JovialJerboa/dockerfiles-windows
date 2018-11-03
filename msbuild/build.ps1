70185dd2ea152e9ab40dbce2ef96fdde41571a75
Liam Flanagan <liam.flanagan@capitalontap.com>
2018-11-03 17:22:02 +0000

    Renamed build-parallel.ps1 build.ps1

--- msbuild/build.ps1
+++ msbuild/build.ps1
@@ -1,18 +1,42 @@
 param (
-    [bool]$PushToRepository = $false
+    [int]$ThrottleLimit = 0
 )
+
+# Define buildContainers workflow
+workflow buildContainers {
+    param (
+        [string]$ScriptPath
+        ,[string[]]$Variants
+        ,[int]$ThrottleLimit
+    )
+    foreach -parallel -throttle $ThrottleLimit ($variant in $Variants) {
+        InlineScript {
+            Write-Output "Building $Using:variant"
+            $variantPath = "$Using:ScriptPath/$Using:variant"
+        
+            # Build docker image
+            docker build -t "lflanagan/msbuild:$Using:variant" -f "$variantPath/Dockerfile" "$variantPath"
+        
+            # Push image(s) to docker hub
+            if ($true -eq $PushToRepository) { docker push "lflanagan/msbuild:$Using:variant" }
+        }
+    }
+}
 
 # Get list of variants
 $variants = Get-ChildItem | Where-Object {$_.PSIsContainer} | Foreach-Object {$_.Name}
 
-foreach ($variant in $variants) {
-    Write-Information "Building $variant"
+# Get script path
+$scriptPath = $PSScriptRoot
 
-    # Build docker image
-    docker build -t "lflanagan/msbuild:$variant" -f "$variant/Dockerfile" "./$variant"
+# Run
+buildContainers -ScriptPath $scriptPath -Variants $variants -ThrottleLimit $ThrottleLimit -ErrorVariable +errors
 
-    # Push image(s) to docker hub
-    if ($true -eq $PushToRepository){
-        docker push "lflanagan/msbuild:$variant"
-    }
+# Error handling
+if ($errors.Count -ne 0) {
+    Write-Output "Errors found."
+    Write-Output $errors
+
+    # Set the exit code to be greater than 0 so the build is marked as failed in AppVeyor
+    if ($env:APPVEYOR -eq $true) { $host.SetShouldExit(1) }
 }
