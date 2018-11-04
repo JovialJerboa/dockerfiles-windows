528a1ff14e47243178749434de5c5e28e8108792
Liam Flanagan <liam.flanagan@capitalontap.com>
2018-11-04 01:13:33 +0000

    Refactored build script to use a json configuration file to build all permutations Added a docker ignore to prevent configuration files from being sent to the docker build context

--- msbuild/build.ps1
+++ msbuild/build.ps1
@@ -6,31 +6,100 @@ param (
 workflow buildContainers {
     param (
         [string]$ScriptPath
-        ,[string[]]$Variants
+        ,[object[]]$BuildDefinitions
         ,[int]$ThrottleLimit
     )
-    foreach -parallel -throttle $ThrottleLimit ($variant in $Variants) {
-        InlineScript {
-            Write-Output "Building $Using:variant"
-            $variantPath = "$Using:ScriptPath/$Using:variant"
-        
-            # Build docker image
-            docker build -t "lflanagan/msbuild:$Using:variant" -f "$variantPath/Dockerfile" "$variantPath"
-        
-            # Push image(s) to docker hub
-            if ($true -eq $PushToRepository) { docker push "lflanagan/msbuild:$Using:variant" }
+    foreach -parallel -throttle $ThrottleLimit ($buildDefinition in $BuildDefinitions) {
+        parallel {
+            $configurations = $buildDefinition.configurations
+
+            # netfx
+            if ($true -eq $configurations.Contains("netfx")) {
+                sequence {
+                    # Variables
+                    $dockerFilePath = "$ScriptPath\netfx"
+                    $tag = "netfx-$($buildDefinition.'netfx-name')"
+                    $netfxTag = $buildDefinition.'netfx-tag'
+
+                    Write-Output "Building $tag"
+
+                    # Build docker image
+                    docker build --build-arg NETFX_TAG=$netfxTag -t "lflanagan/msbuild:$tag" -f $dockerFilePath $ScriptPath 
+
+                    # Push image(s) to docker hub
+                    if ($true -eq $PushToRepository) { docker push "lflanagan/msbuild:$tag" }
+                }
+            }
+
+            # netfx-webtools
+            if ($true -eq $configurations.Contains("netfx-webtools")) {
+                sequence {
+                    # Variables
+                    $dockerFilePath = "$ScriptPath\netfx-webtools"
+                    $tag = "netfx-$($buildDefinition.'netfx-name')-webtools"
+                    $netfxTag = $buildDefinition.'netfx-tag'
+
+                    Write-Output "Building $tag"
+
+                    # Build docker image
+                    docker build --build-arg NETFX_TAG=$netfxTag -t "lflanagan/msbuild:$tag" -f $dockerFilePath $ScriptPath 
+
+                    # Push image(s) to docker hub
+                    if ($true -eq $PushToRepository) { docker push "lflanagan/msbuild:$tag" }
+                }
+            }
+
+            # netfx-dotnet
+            if ($true -eq $configurations.Contains("netfx-dotnet")) {
+                sequence {
+                    # Variables
+                    $dockerFilePath = "$ScriptPath\netfx-dotnet"
+                    $tag = "netfx-$($buildDefinition.'netfx-name')-dotnet-$($buildDefinition.'dotnet-name')"
+                    $netfxTag = $buildDefinition.'netfx-tag'
+                    $dotnetTag = $buildDefinition.'dotnet-tag'
+                    $MSBuildSDKsPath = $buildDefinition.'MSBuildSDKsPath'
+
+                    Write-Output "Building $tag"
+
+                    # Build docker image
+                    docker build --build-arg NETFX_TAG=$netfxTag --build-arg DOTNET_TAG=$dotnetTag --build-arg MSBUILD_SDKS_PATH=$MSBuildSDKsPath -t "lflanagan/msbuild:$tag" -f $dockerFilePath $ScriptPath
+
+                    # Push image(s) to docker hub
+                    if ($true -eq $PushToRepository) { docker push "lflanagan/msbuild:$tag" }
+                }
+            }
+
+            # netfx-dotnet-webtools
+            if ($true -eq $configurations.Contains("netfx-dotnet-webtools")) {
+                sequence {
+                    # Variables
+                    $dockerFilePath = "$ScriptPath\netfx-dotnet-webtools"
+                    $tag = "netfx-$($buildDefinition.'netfx-name')-dotnet-$($buildDefinition.'dotnet-name')-webtools"
+                    $netfxTag = $buildDefinition.'netfx-tag'
+                    $dotnetTag = $buildDefinition.'dotnet-tag'
+                    $MSBuildSDKsPath = $buildDefinition.'MSBuildSDKsPath'
+
+                    Write-Output "Building $tag"
+
+                    # Build docker image
+                    docker build --build-arg NETFX_TAG=$netfxTag --build-arg DOTNET_TAG=$dotnetTag --build-arg MSBUILD_SDKS_PATH=$MSBuildSDKsPath -t "lflanagan/msbuild:$tag" -f $dockerFilePath $ScriptPath 
+
+                    # Push image(s) to docker hub
+                    if ($true -eq $PushToRepository) { docker push "lflanagan/msbuild:$tag" }
+                }
+            }
         }
     }
 }
 
-# Get list of variants
-$variants = Get-ChildItem | Where-Object {$_.PSIsContainer} | Foreach-Object {$_.Name}
+# Get the build definitions
+$buildDefinitions = Get-Content .\build-definitions.json | ConvertFrom-Json
 
 # Get script path
 $scriptPath = $PSScriptRoot
 
 # Run
-buildContainers -ScriptPath $scriptPath -Variants $variants -ThrottleLimit $ThrottleLimit -ErrorVariable +errors
+buildContainers -ScriptPath $scriptPath -BuildDefinitions $buildDefinitions -ThrottleLimit $ThrottleLimit -ErrorVariable +errors
 
 # Error handling
 if ($errors.Count -ne 0) {
