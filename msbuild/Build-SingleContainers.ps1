4e0e4a55e0d304eb13fe2797dd50aca0309c0847
Liam Flanagan <liam.flanagan@capitalontap.com>
2018-11-16 17:14:00 +0000

    Missed some files. Doh!

---
+++ msbuild/Build-SingleContainers.ps1
@@ -0,0 +1,28 @@
+param (
+    [Parameter(Mandatory=$true)][string]$ContainerTag
+    ,[boolean]$PushToRegistry = $false
+)
+
+# Get build arguments
+$scriptPath = $PSScriptRoot
+$buildParameters = . .\Get-BuildParameters.ps1 -ContainerTag $ContainerTag
+$arguments = @{
+    BuildRootPath = $scriptPath
+    ContainerTag = $buildParameters.ContainerTag
+    DockerFile = $buildParameters.DockerFile
+    NetFxName = $buildParameters.NetFxName
+    NetFxTag = $buildParameters.NetFxTag
+    DotNetName = $buildParameters.DotNetName
+    DotNetTag = $buildParameters.DotNetTag
+    MSBuildSDKsPath = $buildParameters.MSBuildSDKsPath
+    PushToRegistry = $PushToRegistry
+}
+
+# Build
+. "$using:ScriptPath\Build-Container.ps1" @arguments
+
+# Error handling
+if ($LASTEXITCODE -ne 0) {
+    # Set the exit code to be greater than 0 so the build is marked as failed in AppVeyor
+    if ($env:APPVEYOR -eq $true) { $host.SetShouldExit(1) }
+}
